a505f5936007b7f778d38b3d343e404d55f24dc3
Setyo <setyo@pm.me>
2023-06-20 14:37:33 +0700

    add tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,34 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { cssBundleHref } from '@remix-run/css-bundle'
+import * as Remix from '@remix-run/react'
+
+import type { LinksFunction } from '@remix-run/node'
+
+import stylesheet from '~/tailwind.css'
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+    ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+    { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
+    { rel: 'stylesheet', href: stylesheet },
+]
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className="h-full">
+            <head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1"
+                />
+                <Remix.Meta />
+                <Remix.Links />
+            </head>
+            <body className="h-full">
+                <Remix.Outlet />
+                <Remix.ScrollRestoration />
+                <Remix.Scripts />
+                <Remix.LiveReload />
+            </body>
+        </html>
+    )
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss'
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+export default {
+    content: ['./app/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [require('@tailwindcss/forms')],
+} satisfies Config
